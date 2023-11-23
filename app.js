1268c2ed94beae67a426f6af814dd2d9a9a691ec
Johannes <johannesmoldestad@gmail.com>
2023-11-23 21:24:09 +0000

    Progress 1

--- app.js
+++ app.js
@@ -17,3 +17,27 @@ app.get('/stocks/:symbol', async (req, res) => {
 })
 
 app.listen(3000, () => console.log('Server is running!'))
+
+async function LoadStocks() {
+  try {
+    const stockArr = await stocks.getStocks()
+    stockArr.forEach(async stock => {
+      let postFix = "Successfully loaded"
+      let loaded = true;
+      let stockData = await stocks.getStockPoints(stock, Date.now()).catch((error) => {postFix = "\nFailed to load"; loaded = false})
+
+      if (!loaded)
+        stockData = "...\n"
+      else
+        stock += ": "
+      
+      console.log(postFix, stock, stockData)
+    })
+  } catch (error) {
+    console.error("Error", error)
+  }
+}
+
+
+
+LoadStocks();
